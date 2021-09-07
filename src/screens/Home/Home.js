a60072378c06a545ec2b486c167f208ee9702c3c
AHMET SELÇUK AZANPA <ahmetazanpa@hotmail.com>
2021-09-08 00:24:19 +0300

    08.09.21 commit

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -1,20 +1,56 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { View } from 'react-native'
-import { Fab,Text } from 'native-base';
+import { Fab, Text, FlatList } from 'native-base';
 import Icon from 'react-native-remix-icon';
+import firestore from '@react-native-firebase/firestore';
 
-const Home = ({navigation}) => {
+const Home = ({ navigation }) => {
+    const [tasks, setTasks] = useState({})
+    const arr = [];
+
+    useEffect(() => {
+        firestore()
+        .collection('tasks')
+        .get()
+        .then(documentSnapshot => {
+            documentSnapshot.docs.forEach(doc => {
+              arr.push(doc.data())
+            })
+        });
+        setTasks(arr)
+    }, [])
+
     return (
         <View>
+            <FlatList
+                data={tasks}
+                renderItem={({ item }) => (
+                    <View style={{ justifyContent: 'space-between', margin:10, backgroundColor: '#d2d2d2', borderRadius: 25}}>
+                            <Text px={5} py={2} rounded="md" my={1} >
+                                {item.taskTitle}
+                            </Text>
+                            <Text px={5} py={2} rounded="md" my={1} >
+                                {item.taskDetail}
+                            </Text>
+                            <Text px={5} py={2} rounded="md" my={1} alignSelf="flex-end" >
+                                {item.user}
+                            </Text>
+                    </View>
+                    
+                )}
+                keyExtractor={(item) => item.id}
+                scrollEnabled
+               
+            />
             <Fab
                 position="absolute"
                 size="sm"
                 label={
                     <Text color="white" fontSize="md">
-                      YENİ GÖREV
+                        YENİ GÖREV
                     </Text>
                 }
-                icon={ <Icon name="ri-add-line" size="30" color="white"/> }
+                icon={<Icon name="ri-add-line" size="30" color="white" />}
                 onPress={() => navigation.navigate('Task')}
             />
         </View>
--- src/screens/Task/Task.js
+++ src/screens/Task/Task.js
@@ -3,6 +3,7 @@ import {
     FormControl,
     Input,
     TextArea,
+    Button,
     Stack,
     Select,
     ScrollView,
@@ -11,35 +12,72 @@ import {
 } from "native-base"
 import firestore from '@react-native-firebase/firestore';
 
+
 const Task = () => {
     const [users, setUsers] = useState([])
     const [user, setUser] = useState("")
+    const [loading, setLoading] = useState(false)
+    const [titles, setTitles] = useState([])
+    const [taskTitle, setTaskTitle] = useState("")
+    const [taskDetail, setTaskDetail] = useState("")
 
     useEffect(() => {
-        const subscriber = firestore()
+        firestore()
           .collection('users')
           .doc('4aSGOqYecSLTwoDOM2Fd')
           .get()
           .then(documentSnapshot => {
-            setUsers(documentSnapshot.data().user)
+              setUsers(documentSnapshot.data().user)
           });
-        // Stop listening for updates when no longer required
-        return () => subscriber();
+
+          firestore()
+          .collection('taskTitle')
+          .doc('CExZSljPUoBV53fOuDR3')
+          .get()
+          .then(documentSnapshot => {
+              setTitles(documentSnapshot.data().title)
+          });
+
       }, []);
-    
+     
+    const addItem = async () => {
+        setLoading(true);
+        await firestore().collection('tasks').add({
+            taskTitle: taskTitle,
+            taskDetail: taskDetail,
+            user: user
+        });
+        setLoading(false);
+    }  
+
     return (
         <ScrollView>
             <Stack space={4} px={4} safeArea mt={6}>
                 <FormControl>
                     <FormControl.Label>Görev Konusu</FormControl.Label>
-                    <Input />
-                    <FormControl.HelperText>
-                        Probleminize ait konuyu seçiniz.
-                    </FormControl.HelperText>
+                    <VStack space={4}>
+                        <Select
+                            selectedValue={taskTitle}
+                            minWidth={200}
+                            accessibilityLabel="Yaşamış olduğunuz problemi seçiniz"
+                            placeholder="Yaşamış olduğunuz problemi seçiniz"
+                            onValueChange={(itemValue) => setTaskTitle(itemValue)}
+                            _selectedItem={{
+                                bg: "cyan.600",
+                                endIcon: <CheckIcon size={4} />,
+                            }}
+                        >
+                        {
+                            titles.map(title => {
+                                return <Select.Item key={title} label={title} value={title} />
+                            })
+                        }
+                        </Select>
+                    </VStack>
                 </FormControl>
                 <FormControl>
-                    <FormControl.Label>Görev Açıklaması(*Yaşamış olduğunuz problemi yazınız.)</FormControl.Label>
-                    <TextArea />
+                    <FormControl.Label>Görev Açıklaması</FormControl.Label>
+                    <TextArea onChangeText={setTaskDetail} value={taskDetail} placeholder="Yaşamış olduğunuz problemi yazınız" />
                 </FormControl>
                 <FormControl>
                     <FormControl.Label>Problemi Yaşayan Kişi</FormControl.Label>
@@ -56,16 +94,16 @@ const Task = () => {
                             }}
                         >
                         {
-                            users.map((user) => {
-                                <Select.Item label={user.nameSurname + "/t(" + user.title + ")" } value={user.nameSurname} />
+                            users.map(user => {
+                                return <Select.Item key={user.nameSurname} label={user.nameSurname + "\t(" + user.title + ")" } value={user.nameSurname} />
                             })
                         }
                         </Select>
                     </VStack>
-                    <FormControl.HelperText>
-                    Listeden İsminizi Seçiniz.
-                    </FormControl.HelperText>
                 </FormControl>
+                <Button isLoading={loading} isLoadingText="Oluştur" variant="solid" onPress={ addItem }>
+                    OLUŞTUR
+                </Button>
             </Stack>
         </ScrollView>
     )
